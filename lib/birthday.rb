f840c6692d038e517aa6459210eac43a478d80c2
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-29 01:32:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    if age < 12
+    if age <= 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     end
   end
