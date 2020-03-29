26465087cfdea801c9b84d50f07ef38216406e26
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-29 01:24:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,6 +8,7 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?('A')
       winner = name
