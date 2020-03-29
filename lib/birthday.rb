9d41ebcd6920b83334049862e8b8366ea6719327
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-29 01:28:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,13 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |name, age|
+    message =  "Happy birthday #{}! You are now #{age} years old!"
+  end
+  message
 end
-
-
